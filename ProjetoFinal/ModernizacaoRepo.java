7ffd18fd2ede14626ff461b5cc848eab4eccbf36
EmersonPaduan <emerson.paduan@gmail.com>
2021-01-21 15:56:51 -0300

    proj final - ModernizacaoRepo

---
+++ ProjetoFinal/ModernizacaoRepo.java
@@ -0,0 +1,19 @@
+package br.paduan.projFinal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import br.paduan.projFinal.model.Comunidade;
+import br.paduan.projFinal.model.Modernizacao;
+
+public interface ModernizacaoRepo extends CrudRepository<Modernizacao, Integer> {
+    public List<Modernizacao> findByComunidadeOrderByDataModernizacao(Comunidade comunidade);
+
+    @Query(value = "select ifnull(round(sum(percentual),2),0) from itmn_modernizacao where id_comunidade = ?1 and year(data_modernizacao) = ?2", nativeQuery = true)
+    public double somaPorcentagem(int idComunidade, int ano);
+
+    @Query(value = "select * from itmn_modernizacao where id_comunidade = ?1 and year(data_modernizacao) = ?2 and month(data_modernizacao) = ?3 LIMIT 1", nativeQuery = true)
+    public Object existe(int idComunidade, int ano, int mes);
+}
